a923f7d7a92bd41c06b071d41996c0e08644c11c
Abaky Viso <abaquihatake@hotmail.com>
2017-08-27 01:27:47 -0600

    - Funcionalidad de Operadores completa - Código Depurado/Optimizado

--- js/app.js
+++ js/app.js
@@ -1,66 +1,139 @@
+// DECLARACION E INICIALIZACION DE VARIABLES GLOBALES
 var arreglo1 = [];
+var cant1 = '';
+var cant2 = '';
+var operacion = '';
 
-// Asignacion de valores al arreglo
+// ASIGNACION DE VALORES A LAS TECLAS CORRESPONDIENTES
 
-var val1 = document.getElementById('1').addEventListener('click', capturar_llenar)
-var val2 = document.getElementById('2').addEventListener('click', capturar_llenar2)
-var val3 = document.getElementById('3').addEventListener('click', capturar_llenar3)
-var val4 = document.getElementById('4').addEventListener('click', capturar_llenar4)
-var val5 = document.getElementById('5').addEventListener('click', capturar_llenar5)
-var val6 = document.getElementById('6').addEventListener('click', capturar_llenar6)
-var val7 = document.getElementById('7').addEventListener('click', capturar_llenar7)
-var val8 = document.getElementById('8').addEventListener('click', capturar_llenar8)
-var val9 = document.getElementById('9').addEventListener('click', capturar_llenar9)
-var val0 = document.getElementById('0').addEventListener('click', capturar_llenar0)
-var valp = document.getElementById('punto').addEventListener('click', capturar_llenar_punto)
-
-
-function capturar_llenar() {
+// 1
+document.getElementById('1').addEventListener('click', function () {
   arreglo1.push(1);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar2() {
+})
+
+// 2
+document.getElementById('2').addEventListener('click', function () {
   arreglo1.push(2);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar3() {
+})
+
+// 3
+document.getElementById('3').addEventListener('click', function () {
   arreglo1.push(3);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar4() {
+})
+
+// 4
+document.getElementById('4').addEventListener('click', function () {
   arreglo1.push(4);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar5() {
+})
+
+// 5
+document.getElementById('5').addEventListener('click', function () {
   arreglo1.push(5);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar6() {
+})
+
+// 6
+document.getElementById('6').addEventListener('click', function () {
   arreglo1.push(6);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar7() {
+})
+
+// 7
+document.getElementById('7').addEventListener('click', function () {
   arreglo1.push(7);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar8() {
+})
+
+// 8
+document.getElementById('8').addEventListener('click', function () {
   arreglo1.push(8);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar9() {
+})
+
+// 9
+document.getElementById('9').addEventListener('click', function () {
   arreglo1.push(9);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar0() {
+})
+
+// 0
+document.getElementById('0').addEventListener('click', function () {
   arreglo1.push(0);
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
-function capturar_llenar_punto() {
+})
+
+// .
+document.getElementById('punto').addEventListener('click', function () {
   arreglo1.push('.');
   document.getElementById('display').innerHTML = arreglo1.join('');
-}
+})
 
 
+// DETERMINAR LA OPERACION MATEMATICA Y ALMACENAR UNA COPIA DEL ARREGLO EN LA VARIABLE CANT1
+
+// SUMA
+document.getElementById('mas').addEventListener('click', function() {
+  operacion = 'suma';
+  cant1 = parseInt(document.getElementById('display').innerHTML); //Mediante Funcion parseInt convertir el string a int
+  document.getElementById('display').innerHTML = '0';
+  arreglo1 = [];
+  console.log('Operacion: ', operacion, 'Primer Valor: ', cant1, 'Nuevo Valor del Arreglo: ', arreglo1);
+})
+
+// RESTA
+document.getElementById('menos').addEventListener('click', function() {
+  operacion = 'resta';
+  cant1 = parseInt(document.getElementById('display').innerHTML); //Mediante Funcion parseInt convertir el string a int
+  document.getElementById('display').innerHTML = '0';
+  arreglo1 = [];
+  console.log('Operacion: ', operacion, 'Primer Valor: ', cant1, 'Nuevo Valor del Arreglo: ', arreglo1);
+})
+
+// MULTIPLICACION
+document.getElementById('por').addEventListener('click', function() {
+  operacion = 'multiplicacion';
+  cant1 = parseInt(document.getElementById('display').innerHTML); //Mediante Funcion parseInt convertir el string a int
+  document.getElementById('display').innerHTML = '0';
+  arreglo1 = [];
+  console.log('Operacion: ', operacion, 'Primer Valor: ', cant1, 'Nuevo Valor del Arreglo: ', arreglo1);
+})
+
+// DIVISION
+document.getElementById('dividido').addEventListener('click', function() {
+  operacion = 'division';
+  cant1 = parseInt(document.getElementById('display').innerHTML); //Mediante Funcion parseInt convertir el string a int
+  document.getElementById('display').innerHTML = '0';
+  arreglo1 = [];
+  console.log('Operacion: ', operacion, 'Primer Valor: ', cant1, 'Nuevo Valor del Arreglo: ', arreglo1);
+})
+
+// FUNCIONALIDAD DEL =
+document.getElementById('igual').addEventListener('click', function() {
+  cant2 = parseInt(document.getElementById('display').innerHTML)
+  console.log('cantidad1:', cant1, 'cantidad2:', cant2);
+  switch (operacion) {
+    case 'suma':
+      var result = calculadora.sumar(cant1, cant2);
+      document.getElementById('display').innerHTML = result;
+      break;
+    case 'resta':
+      var result = calculadora.restar(cant1, cant2);
+      document.getElementById('display').innerHTML = result;
+      break;
+    case 'multiplicar':
+      var result = calculadora.multiplicar(cant1, cant2);
+      document.getElementById('display').innerHTML = result;
+      break;
+    case 'dividir':
+      var result = calculadora.dividir(cant1, cant2);
+      document.getElementById('display').innerHTML = result;
+      break;
+  }
+})
 
 var calculadora = {
 
@@ -81,51 +154,3 @@ var calculadora = {
     return (resultado)
   }
 }
-
-
-/*
-// COPIA Variable Objeto
-var calculadora = function (cant1, cant2) {
-  var resultado = 0;
-
-
-
-  function actualizar_resultado (n_resultado) {
-    resultado = n_resultado;
-  }
-
-  return {
-    sumar: function () {
-      var resultado = cant1 + cant2;
-      actualizar_resultado (resultado)
-    },
-    restar: function () {
-      var resultado = cant1 - cant2;
-      actualizar_resultado (resultado)
-    },
-    multiplicar: function() {
-      var resultado = cant1 * cant2;
-      actualizar_resultado (resultado)
-    },
-    dividir: function() {
-      var resultado = cant1 / cant2;
-      actualizar_resultado (resultado)
-    },
-    resultado: function () {
-      return resultado
-    }
-  }
-
-
-}
-
-*/
-
-
-/*
-function presionar() {
-  document.getElementById('on').style = "width: 10%;"
-}
-*/
-
-//document.getElementById('on').onclick = presionar;
